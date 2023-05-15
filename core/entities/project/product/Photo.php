4cc4d145467f55c01a05cc70216474ae0f1c7f14
Roman <kashik.roman@rambler.ru>
2023-05-15 17:00:35 +0300

    Installed image upload behavior

--- core/entities/project/product/Photo.php
+++ core/entities/project/product/Photo.php
@@ -6,11 +6,14 @@ namespace core\entities\project\product;
 
 use yii\db\ActiveRecord;
 use yii\web\UploadedFile;
+use yiidreamteam\upload\ImageUploadBehavior;
 
 /**
  * @property integer id
  * @property string file
  * @property integer sort
+ *
+ * @mixin ImageUploadBehavior
  */
 class Photo extends ActiveRecord
 {
@@ -49,5 +52,27 @@ class Photo extends ActiveRecord
     public static function tableName(): string
     {
         return '{{%core_photos}}';
+    }
+
+    /**
+     * @return array
+     */
+    public function behaviors(): array
+    {
+        return [
+            [
+                'class'                 => ImageUploadBehavior::class,
+                'attribute'             => 'file',
+                'createThumbsOnRequest' => true,
+                'filePath'              => '@staticRoot/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'fileUrl'               => '@static/origin/products/[[attribute_product_id]]/[[id]].[[extension]]',
+                'thumbPath'             => '@staticRoot/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbUrl'              => '@static/cache/products/[[attribute_product_id]]/[[profile]]_[[id]].[[extension]]',
+                'thumbs'                => [
+                    'admin' => ['width' => 100, 'height' => 70],
+                    'thumb' => ['width' => 640, 'height' => 480],
+                ],
+            ]
+        ];
     }
 }
