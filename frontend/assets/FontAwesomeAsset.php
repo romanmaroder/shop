64ed3c5cb078fd539b2e1d725d4a84c2959deda3
Roman <kashik.roman@rambler.ru>
2023-05-28 21:01:44 +0300

    Placed new layout, integrated assets

---
+++ frontend/assets/FontAwesomeAsset.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace frontend\assets;
+
+
+use yii\web\AssetBundle;
+
+class FontAwesomeAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/plugins/fontawesome-free';
+    public $css = [
+        'css/all.min.css'
+    ];
+}
