7ed8098ea87dba6d4ee059d0fe3894bf89f53552
Roman <kashik.roman@rambler.ru>
2023-06-29 11:10:10 +0300

    Added brand view

---
+++ frontend/views/shop/catalog/brand.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\DataProviderInterface */
+/* @var $brand core\entities\project\Brand */
+
+use yii\helpers\Html;
+
+$this->title = $brand->getSeoTitle();
+
+$this->registerMetaTag(['name' =>'description', 'content' => $brand->meta->description]);
+$this->registerMetaTag(['name' =>'keywords', 'content' => $brand->meta->keywords]);
+
+$this->params['breadcrumbs'][] = ['label' => 'Catalog', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $brand->name;
+?>
+
+    <h1><?= Html::encode($brand->name) ?></h1>
+
+    <hr />
+
+<?= $this->render('_list', [
+    'dataProvider' => $dataProvider
+]) ?>
